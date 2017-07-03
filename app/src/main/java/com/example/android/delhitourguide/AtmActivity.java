6bf26a57393c800867fea3b0c9b1307a6bbd6e9a
TarunISCO <201552063@iiitvadodara.ac.in>
2017-07-03 22:01:48 +0530

    Images updated in ATM activity

--- app/src/main/java/com/example/android/delhitourguide/AtmActivity.java
+++ app/src/main/java/com/example/android/delhitourguide/AtmActivity.java
@@ -14,15 +14,15 @@ public class AtmActivity extends AppCompatActivity {
         setContentView(R.layout.data_list);
 
         ArrayList<DataWord> dataWord = new ArrayList<DataWord>();
-        dataWord.add(new DataWord(getString(R.string.canara_bank), getString(R.string.atm1),R.drawable.atm_machine));
-        dataWord.add(new DataWord(getString(R.string.canara_bank), getString(R.string.atm2),R.drawable.atm_machine));
-        dataWord.add(new DataWord(getString(R.string.icici_bank), getString(R.string.atm3),R.drawable.atm_machine));
-        dataWord.add(new DataWord(getString(R.string.icici_bank), getString(R.string.atm4),R.drawable.atm_machine));
-        dataWord.add(new DataWord(getString(R.string.baroda_bank), getString(R.string.atm5),R.drawable.atm_machine));
-        dataWord.add(new DataWord(getString(R.string.baroda_bank), getString(R.string.atm6),R.drawable.atm_machine));
-        dataWord.add(new DataWord(getString(R.string.pnb), getString(R.string.atm7),R.drawable.atm_machine));
-        dataWord.add(new DataWord(getString(R.string.pnb), getString(R.string.atm8),R.drawable.atm_machine));
-        dataWord.add(new DataWord(getString(R.string.pnb), getString(R.string.atm9),R.drawable.atm_machine));
+        dataWord.add(new DataWord(getString(R.string.canara_bank), getString(R.string.atm1),R.drawable.atm_canara));
+        dataWord.add(new DataWord(getString(R.string.canara_bank), getString(R.string.atm2),R.drawable.atm_canara));
+        dataWord.add(new DataWord(getString(R.string.icici_bank), getString(R.string.atm3),R.drawable.atm_icici));
+        dataWord.add(new DataWord(getString(R.string.icici_bank), getString(R.string.atm4),R.drawable.atm_icici));
+        dataWord.add(new DataWord(getString(R.string.baroda_bank), getString(R.string.atm5),R.drawable.atm_bob));
+        dataWord.add(new DataWord(getString(R.string.baroda_bank), getString(R.string.atm6),R.drawable.atm_bob));
+        dataWord.add(new DataWord(getString(R.string.pnb), getString(R.string.atm7),R.drawable.atm_pnb));
+        dataWord.add(new DataWord(getString(R.string.pnb), getString(R.string.atm8),R.drawable.atm_pnb));
+        dataWord.add(new DataWord(getString(R.string.pnb), getString(R.string.atm9),R.drawable.atm_pnb));
 
         DataAdapter adapter = new DataAdapter(this, dataWord, R.color.color_atm);
         ListView listView = (ListView) findViewById(R.id.list);
